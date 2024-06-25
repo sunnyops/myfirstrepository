7b5f51d17b247c2fb5eb8a92db57392cd66d042e
sunnyops <gurusharansingh2004@gmail.com>
2024-06-25 16:33:02 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Helloworld
+{
+    class Pony
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("write a integer");
+            string str1 = Console.ReadLine();
+            int num = int.Parse(str1);
+            int sum = 0;
+            for (int v = 1; v <= num; v = v + 1) { 
+            
+                sum = sum + v;
+            }
+            Console.Write("the sum of [0 ... " + num + "] " + sum);
+            Console.ReadLine();
+        }
+    }
+}
+                
+
